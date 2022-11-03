2fe2a44f44b06fc223edac64a97e3c9cf2ce4f5f
이예나 <yenalee@MacBook-Pro.local>
2022-11-04 01:41:29 +0900

    [feat] login 기능 구현
    login.js 초안 작성

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,12 @@
-import * as Api from '/api.js';
-import { validateEmail } from '/useful-functions.js';
-
+// import * as Api from '../api';
+// import { JsonWebTokenError } from 'jsonwebtoken';
+// import { validateEmail } from '../useful-functions';
+// import data from './login.json' assert { type: 'json' };
+//console.log(data);
 // 요소(element), input 혹은 상수
 const emailInput = document.querySelector('#emailInput');
 const passwordInput = document.querySelector('#passwordInput');
-const submitButton = document.querySelector('#submitButton');
+const submitButtonLogin = document.querySelector('#submitButtonLogin');
 
 addAllElements();
 addAllEvents();
@@ -14,31 +16,47 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  submitButton.addEventListener('click', handleSubmit);
+  submitButtonLogin.addEventListener('click', handleSubmitLogin);
 }
 
 // 로그인 진행
-async function handleSubmit(e) {
+async function handleSubmitLogin(e) {
   e.preventDefault();
 
   const email = emailInput.value;
   const password = passwordInput.value;
 
   // 잘 입력했는지 확인
-  const isEmailValid = validateEmail(email);
+  // const isEmailValid = validateEmail(email);
   const isPasswordValid = password.length >= 4;
 
-  if (!isEmailValid || !isPasswordValid) {
+  if (!isPasswordValid) {
     return alert('비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.');
   }
 
   // 로그인 api 요청
   try {
-    const data = { email, password };
+    // const data = { email, password };
+    //console.log(data);
+    // const result = await Api.post('/api/login', data);
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
 
-    const result = await Api.post('/api/login', data);
+    let result = await res.json();
+    alert(result.message);
+
     const token = result.token;
-
+    console.log(result);
     // 로그인 성공, 토큰을 세션 스토리지에 저장
     // 물론 다른 스토리지여도 됨
     sessionStorage.setItem('token', token);
@@ -48,7 +66,7 @@ async function handleSubmit(e) {
     // 로그인 성공
 
     // 기본 페이지로 이동
-    window.location.href = '/';
+    window.location.href = './home/home.html';
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
