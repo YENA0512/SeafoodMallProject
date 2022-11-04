7aea8d6aff581023196dc4d4f90c374746a6d8f2
Chris <dnchks08@gmail.com>
2022-11-05 00:22:16 +0900

    [feat] modal 구현
    부트스트랩 모달 사용&구현

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -6,7 +6,7 @@ const packageCondition = document.querySelector('.package_condition');
 const platformCondition = document.querySelector('.platform_condition');
 const deliveryCondition = document.querySelector('.delivery_condition');
 const totalPrice = document.querySelector('.product_total');
-const cartButton = document.querySelector('.insert_cart');
+// const cartButton = document.querySelector('.insert_cart');
 
 // 상품 정보 받아오는 함수
 const getProductData = async () => {
@@ -26,41 +26,6 @@ const getProductData = async () => {
 
   totalPrice.innerHTML = total;
 };
-
-const cartModal = `
-  <div class="modal">
-    <div class="modal_mention">
-      장바구니에 담겼습니다!<br />
-      장바구니로 이동 하시겠습니까?
-    </div>
-    <div class="modal_btn">
-      <button class="continue">쇼핑 계속하기</button>
-      <button class="move" href="">장바구니로 이동</button>
-    </div>
-  </div>
-`;
-
-const moveCart = () => {
-  const modalDiv = document.createElement('div');
-  modalDiv.setAttribute('class', 'modal_layout');
-  modalDiv.innerHTML = cartModal;
-  document.body.prepend(modalDiv);
-  // 모달창 외 영역 클릭시 닫힘
-  modalDiv.addEventListener('click', ({ target }) => {
-    if (target.className === 'modal_layout') {
-      document.body.removeChild(modalDiv);
-    }
-  });
-  // 쇼핑 계속하기 버튼 클릭시 닫힘
-  const cancelButton = document.querySelector('.continue');
-  console.log(cancelButton);
-
-  cancelButton.addEventListener('click', () => {
-    document.body.removeChild(modalDiv);
-  });
-};
-
-cartButton.addEventListener('click', moveCart);
 
 // 모음영역
 addAllElements();
