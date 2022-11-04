b98d54623ac1d08147e73c2d730733c3ad4fd35e
Chris <dnchks08@gmail.com>
2022-11-04 17:43:44 +0900

    [feat] 헤더푸터 layout 완성
    기능 완성 home.html 최하단에 script 생성

---
+++ src/views/assets/layout/footer-layout.js
@@ -0,0 +1,12 @@
+const footerHTML = `
+  <hr />
+  <footer>
+    <div class="footer">
+      <h1>푸터임. 나중에 정할거임 ㅋ</h1>
+    </div>
+  </footer>
+`;
+
+const footerTag = document.createElement('footer');
+footerTag.innerHTML = footerHTML;
+document.body.append(footerTag);
