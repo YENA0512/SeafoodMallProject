721466f3b98f111b8d534d73b7daa56df89f8e69
이예나 <yenalee@MacBook-Pro.local>
2022-11-04 10:39:36 +0900

    [feat] 로그인 기능 초안
    login.js 초안 작성 완료

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,4 +1,4 @@
-// import * as Api from '../api.js';
+import * as Api from '../api.js';
 import { validateEmail } from '../useful-functions.js';
 
 // 요소(element), input 혹은 상수
@@ -6,18 +6,16 @@ const emailInput = document.querySelector('#emailInput');
 const passwordInput = document.querySelector('#passwordInput');
 const submitButton = document.querySelector('#submitButton');
 
-// addAllElements();
-//addAllEvents();
+addAllElements();
+addAllEvents();
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-// async function addAllElements() {
-//   createNavbar();
-// }
+//html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-window.onload = function addAllEvents() {
+function addAllEvents() {
   submitButton.addEventListener('click', handleSubmit);
-};
+}
 
 // 로그인 진행
 async function handleSubmit(e) {
@@ -38,18 +36,18 @@ async function handleSubmit(e) {
   try {
     const data = { email, password };
 
-    //const result = await Api.post('/api/login', data);
-    const res = await fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    let result = await res.json();
-    alert(result.message);
-    console.log(result);
-    console.log(data);
+    const result = await Api.post('/api/login', data);
+    // const res = await fetch('/api/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(data),
+    // });
+    // let result = await res.json();
+    // alert(result.message);
+    // console.log(result);
+    // console.log(data);
     //const token = result.token;
 
     const { token, isAdmin } = result;
