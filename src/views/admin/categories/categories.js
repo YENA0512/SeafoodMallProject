3bf9e0c176591a2ab6191fdc929fa0861fe8219f
Chris <dnchks08@gmail.com>
2022-11-08 02:06:25 +0900

    [feat][docs] 카테고리 수정 api(post) 구현
    카테고리 post api 구현완료
    중복 js파일 모듈화

--- src/views/admin/categories/categories.js
+++ src/views/admin/categories/categories.js
@@ -1,42 +1,68 @@
 import * as Api from '../../api.js';
 
-// const parentCategoryInput = document.querySelector('.parent_category');
-// const childCategoryInput = document.querySelector('.child_category');
-// const speciesInput = document.querySelector('.species');
-// const speciesCodeInput = document.querySelector('.species_code');
-// const speciesImageInput = document.querySelector('.species_image');
+const makeParentCategoryInput = document.querySelector('.make_parent_category');
+const makeChildCategoryInput = document.querySelector('.make_child_category');
+const makeSpeciesInput = document.querySelector('.make_species');
+const makeSpeciesCodeInput = document.querySelector('.make_species_code');
+// const makeSpeciesImageInput = document.querySelector('.make_species_image');
 const getCategoryButton = document.querySelector('.get_categories_btn');
 const showCategories = document.querySelector('.show_categories');
+const makecategoriesButton = document.querySelector('.make_categories');
 
 const getCategoriesList = async () => {
   showCategories.innerHTML = '';
   const res = await Api.get(`/api/v1/categories/list`);
-  const parentCategories = res.data[1].parent_category;
-  const childCategories = res.data[1].child_category;
-
+  const parentCategories = res.data[2].parent_category;
+  const childCategories = res.data[2].child_category;
   childCategories.forEach(async (item) => {
     const response = await Api.get(`/api/v1/categories/${item}`);
     const speciesDetail = response.categoryByChildCategory;
+
+    let i = 1;
     speciesDetail.forEach((el) => {
       showCategories.insertAdjacentHTML(
         'beforeend',
         `
         <li>
           <div>
-            <input class="parent_category" value="${parentCategories}" placeholder="수산물" />
-            <input class="child_category" value="${item}" placeholder="2차" />
-            <input class="species" value="${el.species}" placeholder="품종" />
-            <input class="species_code" value="${el.species_code}" placeholder="코드" />
-            <input class="species_image" value="${el.species_image}" placeholder="이미지" />
+            <input id="modi_parent_category" class="${i}" value="${parentCategories}" placeholder="수산물" />
+            <input id="modi_child_category" class="${i}" value="${item}" placeholder="2차" />
+            <input id="modi_species" class="${i}" value="${el.species}" placeholder="품종" />
+            <input id="modi_species_code" class="${i}" value="${el.species_code}" placeholder="코드" />
+            <input id="modi_species_image" class="${i}" value="${el.species_image}" placeholder="이미지" />
           </div>
           <div>
-            <button>수정</button>
-            <button>삭제</button>
+            <button id="modi_category" class="${i}">수정</button>
+            <button class="${i}">삭제</button>
           </div>
         </li>
         `,
       );
+      i++;
     });
   });
 };
+// 카테고리 생성 api
+const makeCategoriesList = async () => {
+  const parent_category = makeParentCategoryInput.value;
+  const child_category = makeChildCategoryInput.value;
+  const species = makeSpeciesInput.value;
+  const species_code = makeSpeciesCodeInput.value;
+  // const image_path = makeSpeciesImageInput.value;
+  const data = {
+    parent_category,
+    child_category,
+    species,
+    species_code,
+  };
+  await Api.post('/api/v1/categories/', data);
+};
+// 카테고리 수정 api
+// const modifyCategoriesList = async () => {
+//   const res = document.getElementById('1');
+//   console.log(res);
+// };
+// modifyCategoriesList();
+
+makecategoriesButton.addEventListener('click', makeCategoriesList);
 getCategoryButton.addEventListener('click', getCategoriesList);
