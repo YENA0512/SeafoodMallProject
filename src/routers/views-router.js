8560f2921ee0e1db8bab014fad64b119dd989060
Chris <dnchks08@gmail.com>
2022-11-07 20:13:56 +0900

    [feat] product api 연동 완료

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -14,6 +14,7 @@ viewsRouter.use('/mypage', serveStatic('mypage'));
 viewsRouter.use('/mypage-order', serveStatic('mypage-order'));
 viewsRouter.use('/mypage-signout', serveStatic('mypage-signout'));
 viewsRouter.use('/categories/category-search?', serveStatic('categories'));
+viewsRouter.use('/product/:id', serveStatic('product'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
