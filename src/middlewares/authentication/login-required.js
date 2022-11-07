993c37fcb33f15f94188a211c11b583cf64a0c03
gimmicks-u <is@pusan.ac.kr>
2022-11-07 20:11:26 +0900

    [feat][fix] isAdmin 미들웨어, helath check 라우터 추가 , 응답코드 수정
    isAdmin : authorization 미들웨어 추가
    테스트를 위한 health check api추가
    응답코드 수정
    - login require : 실패시 403 -> 401

--- src/middlewares/authentication/login-required.js
+++ src/middlewares/authentication/login-required.js
@@ -8,8 +8,8 @@ const loginRequired = (req, res, next) => {
   // 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
   if (!userToken || userToken === 'null') {
     console.log('서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음');
-    res.status(403).json({
-      result: 'forbidden-approach',
+    res.status(401).json({
+      result: 'un-authorized',
       reason: '로그인한 유저만 사용할 수 있는 서비스입니다.',
     });
 
@@ -22,16 +22,19 @@ const loginRequired = (req, res, next) => {
     const jwtDecoded = jwt.verify(userToken, secretKey);
 
     const userId = jwtDecoded.userId;
+    const userRole = jwtDecoded.role;
 
     // 라우터에서 req.currentUserId를 통해 유저의 id에 접근 가능하게 됨
     req.currentUserId = userId;
+    req.currentUserRole = userRole;
+    console.log(req.currentUserRole, req.currentUserId);
 
     next();
   } catch (error) {
     // jwt.verify 함수가 에러를 발생시키는 경우는 토큰이 정상적으로 decode 안되었을 경우임.
     // 403 코드로 JSON 형태로 프론트에 전달함.
-    res.status(403).json({
-      result: 'forbidden-approach',
+    res.status(401).json({
+      result: 'un-authorized',
       reason: '정상적인 토큰이 아닙니다.',
     });
   }
--- src/middlewares/isAdmin.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const isAdmin = (req, res, next) => {
-  // const userToken = req.headers.authorization?.split(' ')[1];
-};
