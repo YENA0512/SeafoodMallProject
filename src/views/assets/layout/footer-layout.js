f2267453c543cfb6ac8f1dbb00472537453bebcc
Chris <dnchks08@gmail.com>
2022-11-09 01:46:30 +0900

    [feat] footer 제작

--- src/views/assets/layout/footer-layout.js
+++ src/views/assets/layout/footer-layout.js
@@ -5,7 +5,20 @@ const footerHTML = `
   <hr />
   <footer>
     <div class="footer">
-      <h1>푸터임. 나중에 정할거임 ㅋ</h1>
+      <div>
+        <img class="footer_logo" src="/mainlogo.png" />
+        <p>상호명: (주) 오늘바다</p>
+        <p>공동대표: 해3물(3팀)</p>
+        <p>주소: 서울 성동구 아차산로17길 48 성수낙낙 elice-lab 2층 라운지</p>
+        <p>
+          <a href="#">이용약관</a>
+          <em>|</em>
+          <a href="#">개인정보처리방침</a>
+          <em>|</em>
+          <a href="#">이메일무단수집금지</a>
+        </p>
+        <p>Copyright ⓒ 2022 박우찬 설지윤 김영범 김익수 이예나</p>
+      </div>
     </div>
   </footer>
 `;
