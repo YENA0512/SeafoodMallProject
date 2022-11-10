06126492b35ebdf9cb3b680820563488535afcb5
gimmicks-u <is@pusan.ac.kr>
2022-11-10 18:11:32 +0900

    [refactor] 서버 시작 구조변경

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import { server } from './src/app';
 
-import { app } from './src/app';
-import { connectMongoDB } from './src/db';
-import { envValidator } from './src/middlewares/validation/envValidator';
+server.start();
 
-dotenv.config({
-  path: path.resolve(process.env.NODE_ENV === 'production' ? '.production.env' : '.env'),
-});
+// import * as dotenv from 'dotenv';
+// import * as path from 'path';
 
-envValidator();
-connectMongoDB();
+// import { app } from './src/app';
+// import { connectMongoDB } from './src/db';
+// import { envValidator } from './src/middlewares/validation/envValidator';
 
-const PORT = process.env.PORT;
+// dotenv.config({
+//   path: path.resolve(process.env.NODE_ENV === 'production' ? '.production.env' : '.env'),
+// });
 
-app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
-});
+// envValidator();
+// connectMongoDB();
+
+// const PORT = process.env.PORT;
+
+// app.listen(PORT, () => {
+//   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+// });
