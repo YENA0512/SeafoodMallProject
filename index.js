220bbc1ffd6ad63cb71c8d24d0b3f0cc83a43677
ybeom <ybeom.me@gmail.com>
2022-11-10 13:55:33 +0900

    [feat] env const validator 추가
    - packge.json scrpt start:prod 추가. production 환경에서는 별도 env 파일 필요
    - envalid package 추가 완료. 코드 업데이트 후 서버 구동시 npm i 필요.
    - envValidaot middleware 추가. index.js 코드에 추가 완료

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
 import { app } from './src/app';
 import { connectMongoDB } from './src/db';
-// .env 파일에 예를 들어 PORT="3000" 을 작성하면, process.env.PORT가 3000이 됨
-const PORT = process.env.PORT || 5000;
+import { envValidator } from './src/middlewares/validation/envValidator';
 
+dotenv.config({
+  path: path.resolve(process.env.NODE_ENV === 'production' ? '.production.env' : '.env'),
+});
+
+envValidator();
 connectMongoDB();
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
